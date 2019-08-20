e6681fbf9aac64d6b2f51585e5c0f5655689ade0
Fred Astaire <lancebc@thegodardgroup.com>
2019-08-20 12:54:07 -0400

    added preset amounts

--- assets/javascript/tips.js
+++ assets/javascript/tips.js
@@ -2,22 +2,43 @@
 $(document).ready(function () {
     $("#calculateTip").on("click", function () {
         event.preventDefault();
-        var billAmount = $("#billAmount").val().trim();
-        billAmount = parseFloat(billAmount).toFixed(2);
-        console.log(billAmount);
-        var tipPercentage = $("#tipPercentage").val().trim();
-        tipPercentage = parseInt(tipPercentage);
-        console.log(tipPercentage);
-        var result = (billAmount * (tipPercentage / 100));
-        result = parseFloat(result).toFixed(2);
-        console.log(result);
+        var billAmount = parseFloat($("#billAmount").val().trim()).toFixed(2);
+        var tipPercentage = parseInt($("#tipPercentage").val().trim());
+        var result = parseFloat((billAmount * (tipPercentage / 100))).toFixed(2);
         $("#tipAmount").text(result);
-        console.log("This is the original bill: " + billAmount)
-        console.log("This is the calculated tip: " + result)
-        var totalBill = +billAmount + +result;
-        totalBill = parseFloat(totalBill).toFixed(2);
-        console.log(totalBill);
+        var totalBill = parseFloat(+billAmount + +result).toFixed(2);
         $("#billTotal").text(totalBill);
     })
+    $("#clearForm").on("click", function () {
+        location.reload();
+    })
+    $("#tenPercent").on("click", function () {
+        event.preventDefault();
+        var billAmount = parseFloat($("#billAmount").val().trim()).toFixed(2);
+        // var tipPercentage = parseInt($("#tipPercentage").val().trim());
+        var result = parseFloat((billAmount * .1)).toFixed(2);
+        $("#tipAmount").text(result);
+        var totalBill = parseFloat(+billAmount + +result).toFixed(2);
+        $("#billTotal").text(totalBill);
+    })
+    $("#fifteenPercent").on("click", function () {
+        event.preventDefault();
+        var billAmount = parseFloat($("#billAmount").val().trim()).toFixed(2);
+        // var tipPercentage = parseInt($("#tipPercentage").val().trim());
+        var result = parseFloat((billAmount * .15)).toFixed(2);
+        $("#tipAmount").text(result);
+        var totalBill = parseFloat(+billAmount + +result).toFixed(2);
+        $("#billTotal").text(totalBill);
+    })
+    $("#twentyPercent").on("click", function () {
+        event.preventDefault();
+        var billAmount = parseFloat($("#billAmount").val().trim()).toFixed(2);
+        // var tipPercentage = parseInt($("#tipPercentage").val().trim());
+        var result = parseFloat((billAmount * .2)).toFixed(2);
+        $("#tipAmount").text(result);
+        var totalBill = parseFloat(+billAmount + +result).toFixed(2);
+        $("#billTotal").text(totalBill);
+    })
+
 })
 
